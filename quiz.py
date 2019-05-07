a189c8083f36d9c41303c8578edd870de5b9a698
watiri98 <48017838+watiri98@users.noreply.github.com>
2019-05-07 19:18:45 +0300

    Add files via upload
    added my python quiz2 code

---
+++ quiz.py
@@ -0,0 +1,54 @@
+def tens(a):
+	numbers = []
+	for y in a:
+		x = y*10
+		numbers.append(x)
+	return(numbers)		
+def list(b):
+	squares = []
+	x = [y*y for y in b]
+	squares.append(x)
+	return(squares)
+def sorted(c):
+	list1 = []
+	list2 = []
+	for a in list1:
+		for b in list2:
+			x = [a + b]
+			y.append(x)	
+	return (x)
+
+
+def range_sum(d):
+	numbers = []
+	for h in range(n):
+		a = n
+		b = n
+		x = n + n
+		numbers.append(x)
+	return(numbers)
+def largest(a):
+	number = []
+	for z in a:
+		x = g>y
+		number.append(x)
+	return(number)
+def smallest():
+	number = []
+	for b in y:
+		x = b>c
+		number.append(x)
+	return(x)
+def my_modulus():
+	a=[]
+	b=range(10,20)
+	for x in b:
+		if x%3==0:
+			h.append(x)
+	return(x)
+def students():
+	m = dict()
+	for a in students:
+		sms="Hello"{},your balance is ksh{}.format(student["name"],student["balance"])
+		print(sms)
+
